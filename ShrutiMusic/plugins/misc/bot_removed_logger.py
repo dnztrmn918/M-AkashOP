76458f9b30b03de8e368bbcaba33c90fe1f7409b
AkashOP <akp954834@gmail.com>
2025-07-18 12:31:45 +0530

    Update bot_removed_logger.py

--- ShrutiMusic/plugins/misc/bot_removed_logger.py
+++ ShrutiMusic/plugins/misc/bot_removed_logger.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../')))
+
 print("✅ bot_removed_logger.py loaded successfully")
 
 from pyrogram import Client, filters
